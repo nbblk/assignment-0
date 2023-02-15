063b05678f259711245bd922a52e65b406f03fd7
heejin <blake@infrablockchain.com>
2023-02-15 17:57:02 +0900

    set routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {Reading} from './pages/reading';
+import {Writing} from './pages/writing';
+import {Realtime} from './pages/realtime';
+import {Deleting} from './pages/deleting';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <h1>assignment</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Reading />}></Route>
+          <Route path="/read" element={<Reading />}></Route>
+          <Route path="/write" element={<Writing />}></Route>
+          <Route path="/delete" element={<Deleting />}></Route>
+          <Route path="/realtime" element={<Realtime />}></Route>
+        </Routes>
+      
+      </BrowserRouter>  </div>
+ );
 }
 
 export default App;
